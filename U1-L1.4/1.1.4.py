e1c9ff096a1b015824608532930687ce0252438b
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-13 09:09:22 +0800

    冲啊

---
+++ U1-L1.4/1.1.4.py
@@ -0,0 +1,9 @@
+# num_1 = float(input("请输入一个数字 \n"))
+# num_2 = float(input("请输入另一个数字 \n"))
+# sum = num_1 + num_2
+# print(sum)
+#
+# num_3, num_4 , num_5 =1 , 2 , 3
+# print(num_3, num_4 , num_5 , sep= " | " , end= " ")
+
+print("%.4f%%" % 2.2765452)
