2f2d26c8634b7766c51f5cf685837a347dfdc420
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-20 02:14:22 +0800

    A

--- U3-L1/3.1.9.py
+++ U3-L1/3.1.9.py
@@ -1,4 +1,10 @@
 # 3.1.9 质数判断：用户输入一个正整数，用程序判断是否为质数
-i = 2
-while 1:
-    x = int(print("输入一个值"))
+start_number = 2
+x_r = int(input(""))
+while start_number % x_r == 0:
+
+    if x_r == start_number:
+        print("质数")
+        break
+
+    start_number = start_number + 1
