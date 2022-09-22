fb4f7907b9f1d73656d34d52969d99d43901f4e7
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-22 11:56:07 +0800

    NEW

--- Exam/Exam2/4.py
+++ Exam/Exam2/4.py
@@ -1,13 +1,13 @@
 
-    for g in range(1,10):
+for g in range(1,10):
 
-        for s in range(1,10):
+    for s in range(1,10):
 
-            for b in range(1,10):
-                if (g**3 + s**3 + b**3)<1000 or (g**3 + s**3 + b**3)>=100:
-                    print(g,s,b,end="   ",sep="")
-                    b= b+1
-            s = s+1
+        for b in range(1,10):
+            if (g**3 + s**3 + b**3)==g*100+s*10+b:
+                print(g,s,b,end="   ",sep="")
+                b= b+1
+        s = s+1
 
-        g=g+1
+    g=g+1
 
