068530038054039b5c2b7ffbcdf51ebca1708e47
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-22 10:04:06 +0800

    NEW

---
+++ U1-L4/4.1.2.py
@@ -0,0 +1,12 @@
+# 4.1.2 通过定义无参数的函数，简化以下代码
+def mamamato(yangzheng_x,yangzheng_1):
+    intro_1 = yangzheng_x
+    print(yangzheng_1)
+    print('-------------')
+    print("-- loading --")
+    print('-------------')
+    print('\n')
+
+mamamato("hello world!","a")
+mamamato("coding makes me happy!","b")
+mamamato("i lied.","c")
