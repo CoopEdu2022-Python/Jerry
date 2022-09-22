047fd09cef89fb0d9e04cfe5da3c2a6f71ff0382
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-22 12:28:08 +0800

    NEW

--- Exam/Exam2/3.py
+++ Exam/Exam2/3.py
@@ -1,10 +1,14 @@
 m = int(input("请输入一个数字"))
 n = int(input("请输入另外一个数字"))
+x = int(0)
 c = int(0)
-for i in range(1,n):
+d = int(0)
+for i in range(1,n+1):
+
+    for a in range(0,i):
+        c = m*(10**a)
+        x = x + c
 
 
-    print("m"*i)
-
-
+print(x)
 
