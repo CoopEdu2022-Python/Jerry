09fbbce5a5d7190f35517365b5f7ca6ab8747c23
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-26 16:26:31 +0800

    NEW

--- Exam/Exam2/5.py
+++ Exam/Exam2/5.py
@@ -1,18 +1,8 @@
-
-c=0
-for a in range(3,1000):
-    for i in (2 , a):
-        if a % i ==0:
-            if i == a:
-                break
-            elif i != a:
-                while a % i == 0:
-                    c = i + c
-                    if c == a:
-                        print(c)
-                    a = a / i
-                    continue
-
-
-        else: i =i+1
-
+a=0
+for i in range(1,1001):
+    for j in range(1,i):
+        if i % j == 0:
+            a += j
+    if a == i:
+        print(a)
+    a = 0
