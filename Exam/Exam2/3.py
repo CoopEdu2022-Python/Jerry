d0b489c0b862d4fb9ecfb893639a651461902cb9
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-09-26 16:08:15 +0800

    NEW

--- Exam/Exam2/3.py
+++ Exam/Exam2/3.py
@@ -4,11 +4,8 @@ x = int(0)
 c = int(0)
 d = int(0)
 for i in range(1,n+1):
-
     for a in range(0,i):
         c = m*(10**a)
         x = x + c
-
-
 print(x)
 
--- Exam/Exam2/4.py
+++ Exam/Exam2/4.py
@@ -1,9 +1,9 @@
 
-for g in range(1,10):
+for g in range(1,9):
 
-    for s in range(1,10):
+    for s in range(0,10):
 
-        for b in range(1,10):
+        for b in range(0,10):
             if (g**3 + s**3 + b**3)==g*100+s*10+b:
                 print(g,s,b,end="   ",sep="")
                 b= b+1
