0ff4a18fb53aec3d5ec3cd0d812d178b85fd6496
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-10-10 09:22:08 +0800

    先传一部分

--- U1-L5/5.3.6.py
+++ U1-L5/5.3.6.py
@@ -1,14 +1,16 @@
 # 5.3.6 编写一个函数 compare_dict(dict1, dict2)，判断两个字典中存在多少个完全相同的键值对，将重复键值对的数量和重复的键值对保存在一个元祖中，将这个元祖作为返回值
-list_a = {1:3,2:5}
-list_b ={1:3,4:6}
+list_a = {1:3,2:5,8:9}
+list_b ={1:3,4:6,8:9}
 list_d = []
 def compare_dict(dict1, dict2):
-    while 1:
-        for _ in dict2.keys():
-            if dict1.get(_, default=None) == None:
-                continue
-            if dict2[_] == dict1.get():
-                list_d.append({dict2:_})
-            break
+    for _ in dict2.keys():
+        if dict1.get(_) is None:
+            continue
+        if dict2[_] == dict1[_]:
+            list_d.append({_:dict2[_]})
+        continue
+
 compare_dict(list_a,list_b)
+all_tuple = (list_d)
+print(all_tuple)
 #不知道dict1.get咋用
