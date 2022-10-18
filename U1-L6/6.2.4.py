f660e2afe70fae731cc9bea4428386861ef1e1ce
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-10-19 01:31:20 +0800

    尽力了

--- U1-L6/6.2.4.py
+++ U1-L6/6.2.4.py
@@ -1,16 +1,28 @@
-b = [["I",1],['V',5],['X', 10],['L', 50],['C', 100],['D',500],['M',1000]]
 
+def alt(s):
+    list = []
+    s = str(s)
+    s=s.replace('IV','4 ')
+    s=s.replace('IX','9 ')
+    s=s.replace('XL','40 ')
+    s=s.replace('XC','90 ')
+    s=s.replace('CD','400 ')
+    s=s.replace('CM','900 ')
 
-def romanToInt(d):
+    s=s.replace('I','1 ')
+    s=s.replace('V','5 ')
+    s=s.replace('X','10 ')
+    s=s.replace('L','50 ')
+    s=s.replace('C','100 ')
+    s=s.replace('D','500 ')
+    s=s.replace('M','1000 ')
+    end = s.split()
 
-    r = []
+    for _ in range(len(end)):
+        s = int(end[_])
+        list.append(s)
 
-    for _ in range(0,len(d)):
+    a = sum(list)
+    return a
+print(alt("MCMXCIV"))
 
-        if b[-_+1] > b[-_]:
-            a = b[-_+1] - b[-_]
-        else:r.append(b[-_])
-        r.append(a)
-
-    return sum(r)
-romanToInt("VI")
