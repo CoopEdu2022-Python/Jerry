999f37b6c72a2addfc3526bdfc84cdf6ee8c274f
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-10-24 17:05:02 +0800

    成功啦

--- Exam/Exam5/3.2.py
+++ Exam/Exam5/3.2.py
@@ -2,7 +2,7 @@ def alter(n):
     n = int(n)
     if n >2**31:
         print("过头了")
-    print(int(("0b"+(0* (32-(len(str(bin(n)).lstrip("0b"))))+str(bin(n)).lstrip("0b")[::-1])),2))
+    print(int(("0b"+"0"* (32-int(len(str(bin(n)).lstrip("0b"))))+str(bin(n)).lstrip("0b")[::-1]),2))
 
 
 alter(43261596)
