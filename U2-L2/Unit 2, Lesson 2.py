dc54600027048fcc0c780e0cfbebaa066c07cc02
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-10-26 02:03:00 +0800

    成功啦

---
+++ U2-L2/Unit 2, Lesson 2.py
@@ -0,0 +1,22 @@
+# 1. [多行注释] Python 的 self 是什么？
+"""self的意思是对象'自身'"""
+
+# 2. [多行注释] Python 类的内置方法有哪些？
+"""__init__ 
+__new__
+__str__
+__del__ 
+__repr__ 
+__setitem__ 
+__getitem__
+__len__
+__cmp__
+__call__
+__add__
+__sub__
+__mul__
+__truediv__
+__mod__
+__pow__
+"""
+#不知道有什么简单的方法
