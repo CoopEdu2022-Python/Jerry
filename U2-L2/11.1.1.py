7998fbd1664e7889816540339ba7bcec258a8aa0
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-10-27 10:53:28 +0800

    修改

--- U2-L2/11.1.1.py
+++ U2-L2/11.1.1.py
@@ -3,11 +3,6 @@ class Student:
         self.courses = []
         self.id = input("ID")
         self.name = input("name")
-
-    def __del__(self):
-        print("已删除学生：[%s][%s]" % (self.name,self.id))
-
-    def add_courses(self):
         while 1:
             class_choose = input("请输入课程")
             self.courses.append(class_choose)
@@ -16,17 +11,19 @@ class Student:
                 if judge == "Y":
                     break
                 elif judge == "N":
-                    return self.courses
+                    return
                 else:
                     continue
 
+    def __del__(self):
+        print("已删除学生：[%s][%s]" % (self.name, self.id))
 
     def get_course(self):
         print(self.courses)
     def __len__(self):
         return len(self.courses)
 student1 = Student()
-student1.add_courses()
+
 print(student1.name)
 print(student1.courses)
 print(len(student1.courses))
