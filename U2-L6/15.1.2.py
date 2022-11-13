540deea63add8052b10923ec49572d545e64fa2f
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-11-14 04:31:44 +0800

    居然

--- U2-L6/15.1.2.py
+++ U2-L6/15.1.2.py
@@ -1,13 +1,34 @@
 import random
-class FIsh():
+class FIsh:
     def __init__(self):
-        self.x = random.randint(0,10)
+        self.x = random.randint(0, 10)
         self.y = random.randint(0, 10)
-        self.speed = 1
-    def move(self):
-        if random.randint(0,1) == 0:
-            self.x += random.randint(-1,1)
 
+    def move(self):
+        if random.randint(0, 1):
+            self.x += random.randint(-1, 1)
+            self.x = self.set_real(self.x)
+        else:
+            self.y += random.randint(-1, 1)
+            self.y = self.set_real(self.y)
+        print(self.x, self.y)
+        return (self.x,self.y)
+
+    def set_real(self, o):
+        if o > 10:
+            return 9
+        if o < 0:
+            return 1
+        return o
+
+
+    pass
+class Smallfish(FIsh):
+    def __del__(self):
+        print(
+            "被吃了"
+        )
+    def being_eat(self):
 
     pass
 class Bigfish(FIsh):
@@ -15,10 +36,19 @@ class Bigfish(FIsh):
         super().__init__()
         self.Hp = 100
     def move(self):
-        pass
-    pass
+        if random.randint(0, 1):
+            self.x += random.randint(-1, 1)
+            self.x = self.set_real(self.x)
+        else:
+            self.y += random.randint(-1, 1)
+            self.y = self.set_real(self.y)
+        self.Hp -= self.Hp
+        print(self.x, self.y)
+        return (self.x,self.y)
     def eat(self):
-        self.Hp +=20
+        if self.move() == Smallfish.move():
+            for number in range (Smallfish.numbers,0,-1):
+                self.Hp +=20
 
-class Smallfish(FIsh):
-    pass
+
+for _ in range (10):
