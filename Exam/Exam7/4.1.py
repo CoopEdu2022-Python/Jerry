d4b1af790e4cc895001813fcc04ec068623b3543
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-11-14 16:52:00 +0800

    优化移动计算

---
+++ Exam/Exam7/4.1.py
@@ -0,0 +1,16 @@
+def gcd_pro_max_2(*n):
+    n = list(n)
+    n.sort(reverse=True)
+    while n[1] != n[len(n)-1]:
+        for _ in range(0,len(n)-1):
+            if n[_] % n [_+1] == 0:
+                n[_] = n[_+1]
+            else:
+                n[_] = n[_] % n[_+1]
+    return n[len(n)-1]
+
+def gcd_pro_max_1(*n):
+    pass
+print(gcd_pro_max_2(1, 2, 3, 4, 5))
+print(gcd_pro_max_2(2, 4, 6, 8, 10))
+print(gcd_pro_max_2(999, 1999, 2999, 3999, 4999))
---
+++ Exam/Exam7/4.2.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self,health):
+        health = int(health)
+class Manager:
+    def feed(self):
+
+    def perform(self):
+
+    def inspect(self):
+
+
