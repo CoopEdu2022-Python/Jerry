fc9ef63bf8a65209519a0c5c3d672e6e4ecfbb55
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-11-14 17:00:04 +0800

    优化移动计算

--- Exam/Exam7/4.2.py
+++ Exam/Exam7/4.2.py
@@ -2,10 +2,15 @@ class Animal:
     def __init__(self,health):
         health = int(health)
 class Manager:
-    def feed(self):
-
-    def perform(self):
-
-    def inspect(self):
+    def __init__(self,jixiao):
+        self.__jixiao = jixiao
+    def feed(self, animal):
+        animal.health += 20
+        return animal.health
+    def perform(self, animal):
+        animal.health -= 20
+        return animal.health
+    def __inspect(self):
+        return self.jixiao
 
 
