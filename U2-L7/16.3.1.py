80a83ff46cab828984f53b2349adf42c2d334342
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-11-21 14:49:35 +0800

    能跑

--- U2-L7/16.3.1.py
+++ U2-L7/16.3.1.py
@@ -1,11 +1,46 @@
 import random
+safe = []
 class Player():
-    def __init__(self):
-        self.number = 0
+    def __init__(self,number):
+        self.__number = number
     def __del__(self):
-        print("%s号被杀",self.number)
-class Mafia():
-    def __init__(self):
-        self.number = ""
-    def kill(sele):
+        print("%s号被杀",self.__number)
+class Goodpeaple():
+    pass
+
+class Mafia(Player):
+    def __init__(self,*safe:list):
+        self.safe = list(safe)
+    def kill(sele,list_alive):
+        sele.safe.remove((random.randint(0,len(list_alive))))
+
+class Judge():
+
+
+    def initialize(self):
+        x = 0
+        while x < 3:
+            safe = []
+            x = 0
+            for _ in range(1,10):
+                if random.randint(0,1) == 1 or x >= 3:
+                    safe.append(Goodpeaple().__init__(_))
+                else:
+                    safe.append(Mafia().__init__(_))
+                    x += 1
+        return safe
+    def play(self):
+        print("night coming")
+        Mafia.kill()
+        print("day")
+
+
+
+
+judge = Judge
+
+judge.initialize()
+while Goodpeaple not in safe:
+    judge.play()
+
 
