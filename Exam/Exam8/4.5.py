5968815d8dc85e52c0891e3979fe56a86dc76e1f
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-11-23 10:04:34 +0800

    能跑

--- Exam/Exam8/4.5.py
+++ Exam/Exam8/4.5.py
@@ -1,7 +1,10 @@
 def largest_perimeter(nums: list) -> int:
-    nums.sort()
-    if nums[0] + nums[1] <= nums[2]:
-        return 0
-    else:
-        return sum(nums)
-print(largest_perimeter([1,2,1]))
+
+    for i in range(len(nums)-2):
+        nums.sort(reverse=True)
+        if nums[0] + nums[1] > nums[2]:
+            return nums[0]+nums[1]+nums[2]
+        else:
+            nums.pop(nums[1])
+
+print(largest_perimeter([2,1,5,6,9,10,13,7,7,3,2,5,4]))
