93b664a5c9fd01f85f5980827203c9b75d1db1f4
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2022-12-01 10:28:15 +0800

    能跑

---
+++ U2-L12/21.1.7.py
@@ -0,0 +1,7 @@
+def change_name(papername,name):
+    file = open(papername,"r+")
+    file.writelines(name)
+change_name("dldkj.txt","aaaaa")
+change_name("qpeuwr.txt","aaaaa")
+change_name("zmcxbvn.txt","aaaaa")
+
